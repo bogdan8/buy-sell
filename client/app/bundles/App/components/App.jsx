d35a3a601a07b92b0186496c00bc309c2ef9321b
Bogdan <bogdanlitvin8@gmail.com>
2017-01-27 17:02:00 +0200

    [+] tried to give redux state

--- client/app/bundles/App/components/App.jsx
+++ client/app/bundles/App/components/App.jsx
@@ -1,17 +1,82 @@
-import React, { Component } from 'react';
-
-import { Provider } from 'react-redux';
-
-import configureStore from '../containers/configureStore';
-
+import React, {Component} from 'react';
 import Routes from '../routes/Routes';
 
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import './style/App.sass'
+
+var data = [
+  {
+    id: 1,
+    photo: 'https://iso.500px.com/wp-content/uploads/2016/06/stock-photo-142869191-1-1500x1000.jpg',
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    contact: 'John Lennon',
+    category: 'Кіно',
+    price: '4568$'
+  }, {
+    id: 2,
+    photo: 'https://iso.500px.com/wp-content/uploads/2016/06/stock-photo-142869191-1-1500x1000.jpg',
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    contact: 'John Lennon',
+    category: 'Музика',
+    price: '4568$'
+  }, {
+    id: 3,
+    photo: 'https://iso.500px.com/wp-content/uploads/2016/06/stock-photo-142869191-1-1500x1000.jpg',
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    contact: 'John Lennon',
+    category: 'Кіно',
+    price: '4568$'
+  }, {
+    id: 4,
+    photo: 'https://iso.500px.com/wp-content/uploads/2016/06/stock-photo-142869191-1-1500x1000.jpg',
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    contact: 'Paul McCartney',
+    category: 'Ігри',
+    price: '4568$'
+  }, {
+    id: 5,
+    photo: 'https://iso.500px.com/wp-content/uploads/2016/06/stock-photo-142869191-1-1500x1000.jpg',
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    contact: 'John Lennon',
+    category: 'Кіно',
+    price: '4568$'
+  }, {
+    id: 6,
+    photo: 'https://iso.500px.com/wp-content/uploads/2016/06/stock-photo-142869191-1-1500x1000.jpg',
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    contact: 'George Harrison',
+    category: 'Ігри',
+    price: '4568$'
+  }, {
+    id: 7,
+    photo: 'https://iso.500px.com/wp-content/uploads/2016/06/stock-photo-142869191-1-1500x1000.jpg',
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+    contact: 'John Lennon',
+    category: 'Музика',
+    price: '4568$'
+  }
+];
+
+function advertisement(state = data, action) {
+  if (action.type === 'ADD_ADVERTISEMENT') {
+    return [
+      ...state,
+      action.payload
+    ];
+  }
+  return state;
+}
+
+const store = createStore(advertisement, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default class App extends Component {
   render() {
     return (
-      <Routes />
+        <Provider store={store}>
+          <Routes />
+        </Provider>
     );
   }
 }
