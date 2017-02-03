a866df10137127c587a50c337381d695c9b7d295
Oleh Demyanyuk <olkobg@gmail.com>
2017-02-03 17:44:17 +0200

    [+] Secured auth for user and jwt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125165007) do
+ActiveRecord::Schema.define(version: 20170203153449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170125165007) do
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string "telephone"
     t.uuid   "role_id"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.string "username"
   end
