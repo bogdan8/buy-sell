1fa2babfd46ae8199e37a13c17d348b0e01aa89b
Oleh Demyanyuk <olkobg@gmail.com>
2017-02-03 17:51:25 +0200

    [+] Implemented controller user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Knock::Engine => '/auth'
+
   get '*path' => 'static#index', constraints: lambda { |req| req.format != 'json' }
+
+  resources :users;
 end
