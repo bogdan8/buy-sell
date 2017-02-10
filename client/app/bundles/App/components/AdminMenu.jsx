b69f0f6dd725491736a4bff687271e2b4e3a0766
Bogdan <bogdanlitvin8@gmail.com>
2017-02-10 10:07:38 +0200

    [+] add mdl Menu

--- client/app/bundles/App/components/AdminMenu.jsx
+++ client/app/bundles/App/components/AdminMenu.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {Menu, MenuItem} from 'react-mdl';
 
 export default function AdminMenu() {
   return (
-      <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-          htmlFor="demo-menu-lower-right">
-        <li className="mdl-menu__item">
-          <Link to="/sign_in" className="mdl-menu__item mdl-js-ripple-effect">Ввійти</Link>
-        </li>
-        <li className="mdl-menu__item">
-          <Link to="/register" className="mdl-menu__item mdl-js-ripple-effect">Зареєструватись</Link>
-        </li>
-        <li className="mdl-menu__item">
-          <Link to="/products" className="mdl-menu__item mdl-js-ripple-effect">Оголошення</Link>
-        </li>
-        <li className="mdl-menu__item">
-          <Link to="/admin/user" className="mdl-menu__item mdl-js-ripple-effect">Таблиця
-            користувачів</Link>
-        </li>
-        <li className="mdl-menu__item">
-          <Link to="/admin/categories" className="mdl-menu__item mdl-js-ripple-effect">Таблиця
-            рубрик</Link>
-        </li>
-        <li className="mdl-menu__item">
-          <Link to="/admin/new_products" className="mdl-menu__item mdl-js-ripple-effect">Нові
-            оголошення</Link>
-        </li>
-      </ul>
+      <Menu
+          align="right"
+          target="demo-menu-lower-right"
+          ripple
+      >
+        <MenuItem>
+          <Link to="/sign_in">
+            Ввійти
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/register">
+            Зареєструватись
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/products" >
+            Оголошення
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/admin/user">
+            Таблиця користувачів
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/admin/categories">
+            Таблиця рубрик
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/admin/new_products">
+            Нові оголошення
+          </Link>
+        </MenuItem>
+      </Menu>
   );
 }
--- client/app/bundles/App/components/UserMenu.jsx
+++ client/app/bundles/App/components/UserMenu.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {Menu, MenuItem} from 'react-mdl';
 
 export default function UserMenu() {
   return (
-      <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-          htmlFor="demo-menu-lower-right">
-        <li className="mdl-menu__item">
-          <Link to="/sign_in" className="mdl-menu__item mdl-js-ripple-effect">Ввійти</Link>
-        </li>
-        <li className="mdl-menu__item">
-          <Link to="/register" className="mdl-menu__item mdl-js-ripple-effect">Зареєструватись</Link>
-        </li>
-        <li className="mdl-menu__item">
-          <Link to="/products" className="mdl-menu__item mdl-js-ripple-effect">Оголошення</Link>
-        </li>
-      </ul>
+      <Menu
+          align="right"
+          target="demo-menu-lower-right"
+          ripple
+      >
+        <MenuItem>
+          <Link to="/sign_in">
+            Ввійти
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/register">
+            Зареєструватись
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/products" >
+            Оголошення
+          </Link>
+        </MenuItem>
+      </Menu>
   );
 }
