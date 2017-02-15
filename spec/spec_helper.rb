8b962c86191c52353bcbac22818310059a000679
Bogdan <bogdanlitvin8@gmail.com>
2017-02-15 13:17:18 +0200

    [~] edit secured_resources test and add factory_girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
+require 'support/factory_girl'
 require 'phantomjs'
 require 'capybara/poltergeist'
 
