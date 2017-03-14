65c3e633c0d87ca5ff40ef4da0535973b43887d8
Bogdan <bogdanlitvin8@gmail.com>
2017-03-14 16:07:47 +0200

    [~] change position notification and bug fix

--- client/app/bundles/App/containers/Notification.jsx
+++ client/app/bundles/App/containers/Notification.jsx
@@ -15,10 +15,11 @@ class NotificationContainer extends Component {
   }
 
   componentWillReceiveProps(newProps) {
-    const {message, level} = newProps.notification;
+    const {message, level, position} = newProps.notification;
     this.notificationSystem.addNotification({
       message,
-      level
+      level,
+      position
     });
   }
 
