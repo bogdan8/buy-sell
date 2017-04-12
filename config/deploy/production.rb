a0ca21e5b3ac04f79c09f87db6cd700c335f09a0
bogdan <bogdanlitvin8@gmail.com>
2017-04-12 19:54:55 +0300

    [+] add set user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,16 +1,17 @@
 set :stage, :production
 set :branch, 'master'
+set :user, 'bobo'
 
 role :app, %w{bobo@127.0.0.1}
 role :web, %w{bobo@127.0.0.1}
 
-server '127.0.0.1', user: 'bobo', roles: %w{app, web}, primary: true
+server '127.0.0.1', user: fetch(:user), roles: %w{app, web}, primary: true
 
-set :deploy_to, '/home/bobo/fshop'
+set :deploy_to, "/home/#{fetch(:user)}/fshop"
 
 set :rails_env, :production
 
-set :ssh_options, { forward_agent: true, user: 'bobo', keys: %w(~/.ssh/id_rsa.pub), port: 3023 }
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub), port: 3023 }
 
 set :puma_init_active_record, true
 
